3fefc856d73c53102422bfbcf1aebcc2aef879ef
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-03-18 21:12:08 +0530

    chore: learn about variables

--- basics-of-go/variables/variables.go
+++ basics-of-go/variables/variables.go
@@ -1,7 +1,20 @@
-package basicsofgo
+package main
 
-// import "fmt"
+import "fmt"
+
+//uint --> unsigned integer --> means just positive integer
+// int --> signed integer --> means just positive and negative integer
+//float --> means just floating point number
+//bool --> means just true or false
+// string --> means just text
+//rune --> means just character (rune(8 bits) == int32)
+// byte --> means just byte of memory  (byte(8 bits) == uint8)
+
+// unint8 --> means just 8 bits of memory
+// uint16 --> means just 16 bits of memory
 
 func main(){
-	// fmt.Println("")
+	var x string ="hello world bhai";
+	fmt.Println(x)
+
 }
