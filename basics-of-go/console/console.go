5cb2e3cf58f7ad781b26d846af7e083ecbc803ef
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-03-21 19:29:00 +0530

    feat: add initial console example in Go demonstrating variable usage and formatting

---
+++ basics-of-go/console/console.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	x:=3
+	// prints them adding spaces with new line /n by default
+	fmt.Println("hello",x,2)
+
+
+	// y:=false
+
+	// %T prints the types of the variable 
+	// fmt.Printf("%T %T",y,y)
+
+
+	z:=3453453534534543535
+	//represents %b the binary format of variable
+	fmt.Printf("%b", z)
+}
