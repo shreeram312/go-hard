59a9c85f68e97b4338d344b2591bcef756606aee
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-03-27 19:38:06 +0530

    feat: add while loop example in Go using for loop syntax for iteration

--- basics-of-go/loops/main.go
+++ basics-of-go/loops/main.go
@@ -3,7 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	for idx:=0; idx <10; idx++ {
-		fmt.Println(idx)
+	//for loop
+	// for idx:=0; idx <10; idx++ {
+	// 	fmt.Println(idx)
+	// }
+
+
+	// while loop
+	// there is no while loop in go but we can use for loop as while loop
+
+	//while loop
+	a:=1
+	for a<=10{
+		fmt.Println(a)
+		a++
 	}
+
+
 }
