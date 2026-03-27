8efe8c50aa138a7713c4338644d86b389246fb28
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-03-27 19:20:02 +0530

    refactor: comment out unused code and enhance switch case examples in Go

--- basics-of-go/condtions/main.go
+++ basics-of-go/condtions/main.go
@@ -5,11 +5,10 @@ import "fmt"
 
 
 func main(){
-	x:= uint8(8)
-	y:=int(5)
-	z:= int(x)==y
-	fmt.Println(z)
-
+	// x:= uint8(8)
+	// y:=int(5)
+	// z:= int(x)==y
+	// fmt.Println(z)
 
 	// we have && || 
 	abc:=10
@@ -18,6 +17,34 @@ func main(){
 	}else
 	{
 		fmt.Println("lesser")
+	}
+
+
+	a:=10
+	// switch a{
+	// case 10:
+	// 	fmt.Println("a")
+	// case 20:
+	// 	fmt.Println("twenty")
+	// default:
+	// 	fmt.Println("not a number")
+	// }
+
+
+
+	//different type of switch case naked switch case
+
+
+	//in go there is break statement by default so we dont need to write break statement in switch case
+	switch {
+	case a > 6:
+		fmt.Println("a is greater than 10")
+		fallthrough // it will execute the next case even if the condition is not met
+    case a > 8:
+	fmt.Println("a is greater than 8")
+
+	default:
+		fmt.Println("a is not greater than 10 or 8")
 	}
 	
 
