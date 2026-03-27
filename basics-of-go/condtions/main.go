aeb12d0b8062b7e25512465a5401df18e734a5fd
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-03-27 19:09:35 +0530

    feat: add conditional statements and comparisons in Go example

---
+++ basics-of-go/condtions/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+
+
+func main(){
+	x:= uint8(8)
+	y:=int(5)
+	z:= int(x)==y
+	fmt.Println(z)
+
+
+	// we have && || 
+	abc:=10
+	if abc > 100 {
+		fmt.Println("greater")
+	}else
+	{
+		fmt.Println("lesser")
+	}
+	
+
+	
+}
