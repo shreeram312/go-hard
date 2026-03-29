db260fcca66aa584d664e505573711ba0b7918f6
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-03-29 15:01:32 +0530

    feat: add string loop example in Go demonstrating UTF-8 character handling

--- basics-of-go/loops/main.go
+++ basics-of-go/loops/main.go
@@ -13,11 +13,32 @@ func main() {
 	// there is no while loop in go but we can use for loop as while loop
 
 	//while loop
-	a:=1
-	for a<=10{
-		fmt.Println(a)
-		a++
-	}
+	// a:=1
+	// for a<=10{
+	// 	fmt.Println(a)
+	// 	a++
+	// }
+
+//string loops
+
+const str string ="he🙂"
+
+fmt.Println(len(str))
+
+// ascii --> 1 byte 256 characters --> 0-255
 
 
+
+// utf-8 --> 4 byte 1114112 characters --> 0-1114111
+
+// genenrally all special characters like emois and hindi characters are represented in utf-8
+
+
+// even if u access the index value it will give the ascii value of the character or utf bytes storage size for emojis
+
+
+	for idx:=0; idx<len(str); idx++ {
+	fmt.Println(string(str[idx]))
+	
+}
 }
