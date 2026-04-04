be658ee690ba505d06e06f2e2bb446e55d7ad243
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-04-04 12:48:52 +0530

    feat: implement slice modification example in Go demonstrating function parameter passing

--- basics-of-go/slices-8/main.go
+++ basics-of-go/slices-8/main.go
@@ -10,8 +10,29 @@ func main(){
 	// length --> numbers of elements in the slice
 	// capacity --> number of 
 
-	  arr:=[5] int {1,2,3,4,5}
-	  sl:= arr[1:3]
-	  sl = sl[:4]
-	  fmt.Println(sl, arr , len(sl), cap(sl))
+	//   arr:=[5] int {1,2,3,4,5}
+	//   sl:= arr[1:3]
+	//   sl = sl[:4]
+	//   fmt.Println(sl, arr , len(sl), cap(sl))
+
+	// sl:= make([]int, 10, 10)
+
+
+	sl:=[]string {"hello", "world", "how", "are", "you"}
+
+
+	// for _, val:= range sl{
+	// 	fmt.Println( val)
+	// }
+
+     test(sl)
+	 fmt.Println(sl)
+
+	 // in this case the slice is passed in fucntion so the slice can be modified which was not possible in arrays
+	
+}
+
+
+func test(sl []string){
+	sl[0]= "test"
 }
