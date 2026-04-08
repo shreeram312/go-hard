3d4608eb41f2381a7b49193f2721a44b3da91035
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-04-08 19:35:00 +0530

    feat: add functions demonstrating basic operations, callbacks, and closures in Go

---
+++ basics-of-go/functions-10/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+func add(num1 int , num2 int) (int, string){
+	return num1+ num2 , "hello world"
+}
+
+
+
+func callFunc( callable func(int) int) int {
+	return callable(10)
+}
+
+
+
+func doubleNumber(num int) int{
+	return num * 2
+
+}
+
+
+func tripleNumber(num int) int{
+	return num * 3
+}
+
+
+
+func firstFunc(str string)  func(string) string {
+	return func( str2 string ) string	 {
+		return str + " " + str2
+	}
+}
+
+
+
+func main(){
+	 value, str:=add(10,200)
+	 fmt.Println(value , str)
+
+
+// callback function similar to javascript
+	 value2 := callFunc(tripleNumber)
+	 fmt.Println(value2)
+
+
+
+	 // annonymous function
+	 value3:= callFunc(func(x int) int{
+		return x * 4
+	 })
+
+	 fmt.Println(value3)
+
+
+
+	 f1:= firstFunc("hello")
+	 f2:= f1("world")
+
+	 fmt.Println(f2)
+	 
+}
