d5589d3e491305953a95d00f016233f8700e21bb
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-04-14 19:42:45 +0530

    refactor: update Person struct fields to be public and modify associated methods for name manipulation

--- basics-of-go/structs-11/main.go
+++ basics-of-go/structs-11/main.go
@@ -7,28 +7,31 @@ import "fmt"
 
 
 type Person struct  {
-	name string
-	age uint
+
+	//setting first letter of the field name to capital to make it public  and can be accessed outside the package
+
+	Name string
+	Age uint
 }
 
 
-func (p Person ) getName()  string{
-	return p.name
+func (p Person ) GetName()  string{
+	return p.Name
 }
 
-func (p Person ) setName(name string) {
-	p.name = name
-	fmt.Println(p.name)
+func (p Person ) SetName(name string) {
+	p.Name = name
+	fmt.Println(p.Name)
 }
 
 
 func main() {
-	var p1  Person = Person{ name: "John", age: 20,}
+	var p1  Person = Person{ Name: "John", Age: 20,}
 
 	// p1 is  sent as a copy of the struct so the name is not changed
 
 	// so it cant be modified the original struct
 	// ideally can be done using pointers
-	p1.setName("Jane")
-	fmt.Println(p1.getName())
+	p1.SetName("Jane")
+	fmt.Println(p1.GetName())
 }
