4f808ad988bd70d75d3652484007f6fcb46c4d90
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-06-04 11:20:23 +0530

    feat: update main.go to demonstrate struct usage and method implementation with Book type

--- basics/pointers-13/main.go
+++ basics/pointers-13/main.go
@@ -3,10 +3,41 @@ package main
 import "fmt"
 
 
-func main(){
-	x:=0
-	y:= &x
+// func main(){
+// 	x:=0
+// 	y:= &x
 
-	*y=100
-	fmt.Println(x,*y)
+// 	*y=100
+// 	fmt.Println(x,*y)
+// }
+
+
+// func change(num *int){
+// 	*num = 100;
+// }
+
+// func main(){
+// 	x:=10
+
+// 	change(&x)
+// 	fmt.Println(x)
+// }
+
+
+type Book struct {
+	id int
+	title string
+}
+
+
+func (b *Book) setTitle(title string){
+	b.title  = title
+
+}
+
+
+func main(){
+	b:= Book{10, "Old"}
+	b.setTitle("New")
+	fmt.Println(b)
 }
