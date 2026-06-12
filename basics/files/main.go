29a8afd3ffa3f18dab290a240308d5345e8aef42
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-06-13 05:00:52 +0530

    feat: update main.go to implement file reading functionality, including error handling and displaying byte data from example.txt

--- basics/files/main.go
+++ basics/files/main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
-	"fmt"
+	
 	"os"
 )
 
 func main(){
+	// f, err := os.Open("basics/files/example.txt")
+	// if err!=nil{
+	// 	panic(err)
+	// }
+
+	// fileInfo,err:=f.Stat()
+	// if err!=nil{
+	// 	panic(err)
+	// }
+	// fmt.Println(fileInfo.Name())
+	// fmt.Println(fileInfo.IsDir())
+	// fmt.Println(fileInfo.ModTime())
+
+
+	// read file
+
 	f, err := os.Open("basics/files/example.txt")
+
+	if err!= nil {
+		panic(err)
+	
+	}
+
+	defer f.Close()
+
+	buf:=make([] byte,30)
+
+
+	d, err:= f.Read(buf)
+
 	if err!=nil{
 		panic(err)
 	}
 
-	fileInfo,err:=f.Stat()
-	if err!=nil{
-		panic(err)
+
+	for i:=0;i<len(buf);i++{
+		println(string(buf[i]),d)
 	}
-	fmt.Println(fileInfo.Size())
+
+
+
+
+
 }
