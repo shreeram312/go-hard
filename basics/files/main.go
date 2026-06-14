52ef8a1baf32874edcc16e988665d4a16a4db683
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-06-14 16:20:33 +0530

    refactor: comment out file reading logic in main.go and add file creation functionality with example content

--- basics/files/main.go
+++ basics/files/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	
+
 	"os"
+
 )
 
 func main(){
@@ -22,29 +23,66 @@ func main(){
 
 	// read file
 
-	f, err := os.Open("basics/files/example.txt")
+	// f, err := os.Open("basics/files/example.txt")
 
-	if err!= nil {
-		panic(err)
+	// if err!= nil {
+	// 	panic(err)
 	
-	}
+	// }
 
-	defer f.Close()
+	// defer f.Close()
 
-	buf:=make([] byte,30)
+	// buf:=make([] byte,30)
 
 
-	d, err:= f.Read(buf)
+	// d, err:= f.Read(buf)
 
+	// if err!=nil{
+	// 	panic(err)
+	// }
+
+
+	// for i:=0;i<len(buf);i++{
+	// 	println(string(buf[i]),d)
+	// }
+
+
+	// f, err := os.ReadFile("basics/files/example.txt") // not to use Readfile generally coz it loads all the data once in the memory 
+	// if err!=nil{
+	// 	panic(err)
+	// }
+
+	// fmt.Println(string(f))
+
+
+	// // read folder
+
+	// dir, err := os.Open("../")
+
+	// if err!=nil{
+	// 	panic(err)
+	// }
+
+	// defer dir.Close()
+
+	// fileInfo ,err:=dir.ReadDir(2)
+
+	// for _, fi :=range fileInfo {
+	// 	fmt.Println(fi.Name())
+	// }
+
+
+	// create a file
+
+	f,err:= os.Create("example2.txt")
 	if err!=nil{
 		panic(err)
 	}
 
+	defer f.Close()
 
-	for i:=0;i<len(buf);i++{
-		println(string(buf[i]),d)
-	}
-
+	f.WriteString("hi go bro")
+	f.WriteString(" Hello")
 
 
 
