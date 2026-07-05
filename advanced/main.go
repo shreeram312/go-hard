7341ab0a4dbbf3768835979968c7419e15583ee9
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-07-05 15:14:15 +0530

    feat: add truck processing functionality in main.go, including error handling and logging for each truck

---
+++ advanced/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type Truck struct{
+	id string
+}
+
+
+
+func processTruck(truck *Truck)error {
+	truck.id ="23"
+	fmt.Println("Processing truck",truck)
+
+
+	return errors.New("Some error")
+	
+}
+
+
+func main(){
+	truck:=[]Truck{
+		{id: "10"},
+		{id:"20"},
+		{id:"30"},
+	}
+
+
+
+	for i := range truck{
+		
+		err:=processTruck(&truck[i])
+
+		if err!=nil{
+			log.Fatalf("Error processing %s",err)
+		}
+		fmt.Println("ok")
+	}
+}
