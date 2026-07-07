822910f2546cde563dc2eb4d2eff7eb92ef16018
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-07-07 13:15:01 +0530

    refactor: implement Truck interface and enhance cargo loading/unloading methods in main.go, improving truck processing logic and structure

--- advanced/main.go
+++ advanced/main.go
@@ -1,72 +1,97 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 )
 
-type Truck struct{
+
+type Truck interface {
+	LoadCargo() error
+	UnloadCargo() error
+}
+
+type NormalTruck struct{
 	id string
-
+	cargo int
 }
 
 
-func (t *Truck) LoadCargo() error{
+type ElectricTruck struct{
+	id string
+	cargo int
+	battery float64
+}
+
+
+func (e *ElectricTruck) LoadCargo() error{
+	e.cargo+=1
 	return nil
+}
 
+
+func (e *ElectricTruck) UnloadCargo() error{
+	e.cargo=0
+	return nil
+}
+
+
+
+func (t *NormalTruck) LoadCargo() error{
+	t.cargo+=10
+	return nil
+}
+
+func (t *NormalTruck) UnloadCargo() error{
+	t.cargo=0
+	return nil
 }
 
 
 
 
-var (
-	ErrNotImplemented  = errors.New("Not Implemented ")
-	ErrTruckNotFound = errors.New("Truck not found")
-)
-
-func processTruck(truck Truck)error {
-
-	fmt.Println("Processing truck",truck)
 
 
 
-	return ErrNotImplemented
-	
+// var (
+// 	ErrNotImplemented  = errors.New("Not Implemented ")
+// 	ErrTruckNotFound = errors.New("Truck not found")
+// )
+
+func processTruck(truck  Truck) error {
+	fmt.Printf("Processing truck %+v \n",truck)	
+
+	err:= truck.LoadCargo()
+	if err!=nil{
+		return fmt.Errorf("Error Loading")
+	}
+	return nil
 }
 
 
 func main(){
-	truck:=[]Truck{
-		{id: "10"},
-		{id:"20"},
-		{id:"30"},
+	truck:=&NormalTruck{
+	id: "10",
+	cargo: 20,
+	}
+
+	electricTruck:=&ElectricTruck{
+		id: "12",
+		cargo: 10,
+		battery: 22.33,
 	}
 
 
+	processTruck(truck)
 
-	for i := range truck{
-		
-		// err:=processTruck(&truck[i])
+	processTruck(electricTruck)
+	
 
-		// if err!=nil{
-		// 	log.Fatalf("Error processing %s",err)
-		// }
+	log.Println(truck.cargo)
+	log.Println(electricTruck.cargo)
 
 
-		// another way
-		if err:=processTruck(truck[i]); err!=nil{
-			// if errors.Is(err,ErrNotImplemented){
-			// 	// we do this 
-			// }
 
-			// if errors.Is(err,ErrTruckNotFound){
-			// 	// we do this 
-			// }
 
-			// or use switch
-			log.Fatalf("Error processing %s",err)
-		}
-		fmt.Println("ok")
-	}
+
 }
